ba7f7d6ce29f8cd70a95914c0974766217e96a57
IgnacioBinaghi <binaghiignacio@gmail.com>
2022-06-19 16:41:37 -0400

    Update 1

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#open').click(function(){
+    $('.container').css("opacity","1");
+    $('.container').css("pointer-events","auto");
+  });
+  $('#login').click(function(){
+    $('.container').css("opacity","0");
+    $('.container').css("pointer-events","none");
+  });
+});
