131ac8f8a1c5a83ee2d219ae53995006ef8764fa
Tomi Binaghi <thomasbinaghi@gmail.com>
2022-06-22 20:56:32 -0400

    okok

--- wsAlpha.py
+++ wsAlpha.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 import addToNewsletterList, sys
 import bcrypt
 
 app = Flask(__name__)
-
+app.secret_key = "hello"
 
 @app.route("/")
 @app.route("/home")
@@ -12,20 +12,20 @@ def home():
 
 
 @app.route("/register")
-def result():
+def register():
     return render_template('register.html')
 
 @app.route("/registered", methods=["POST", "GET"])
-def register():
+def registered():
     output = request.form.to_dict()
     email = output["email"]
     addToNewsletterList.add_mailjet(email)
+    flash('You have registered successfully', 'info')
     return render_template("index.html")
 
 @app.route("/dashboard")
 def profile():
     return render_template('dashboard.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
